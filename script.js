4acf3816601aaacba60fc5bf61d6367a03da2462
“PriscilaDuque” <priscila.o.vaz@hotmail.com>
2022-01-14 17:49:01 -0300

    Criar consumo de API Rick And Morty

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ const imagem = document.querySelector('img');/** Textos com aspas simples são m
 const botao = document.querySelector('button');/**Textos com aspas duplas são menos manipuláveis */
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status'); 
+const condicao = document.querySelector('#status'); 
+
+
+pegarPersonagem = () => {
+  return fetch(`https://rickandmortyapi.com/api/character/2`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      "Content-type": 'application/json'
+    }
+  }).then((response) => response.json()).then((data) => {
+    imagem.src = data.image;
+    imagem.alt = data.name;
+    nomeDoPersonagem.innerHTML = data.name;
+    especie.innerHTML = data.especies;
+    condicao.innerHTML = data.status;
+
+  });
+}
+botao.onclick = pegarPersonagem;
+
