d8a7509996eabb8f210b3a2778e2ab0699b90dfd
“PriscilaDuque” <priscila.o.vaz@hotmail.com>
2022-01-14 16:56:11 -0300

    Capturar elementos HTML manipuláveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');/** Textos com aspas simples são mais manipulavéis */
+const botao = document.querySelector('button');/**Textos com aspas duplas são menos manipuláveis */
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status'); 
